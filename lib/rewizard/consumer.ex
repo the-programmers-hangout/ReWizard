918770415c4ddc1897e026973e55082076cbb54a
Hamza Ali <me@hamzantal.pw>
2020-10-26 09:27:26 +0700

    match msg content with regex so it is mentionable on mobile

--- lib/rewizard/consumer.ex
+++ lib/rewizard/consumer.ex
@@ -36,7 +36,7 @@ defmodule Rewizard.Consumer do
   end
 
   def handle_event({:MESSAGE_CREATE, msg, _ws}) do
-    if msg.content == "<@!#{Me.get().id}>" do
+    if msg.content =~ ~r/<@!?#{Me.get().id}>/ do
       Rewizard.Cogs.Info.command(msg, [])
     else
       CommandInvoker.handle_message(msg, CommandStorage)
