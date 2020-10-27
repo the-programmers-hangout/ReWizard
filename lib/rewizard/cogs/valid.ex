32a56275f2cab4f688d7b7c7e7ae179aad7459b9
Hamza Ali <me@hamzantal.pw>
2020-10-27 12:24:12 +0700

    move `valid.ex` to use common `Rewizard.Regex`

--- lib/rewizard/cogs/valid.ex
+++ lib/rewizard/cogs/valid.ex
@@ -6,7 +6,7 @@ defmodule Rewizard.Cogs.Valid do
   import Nostrum.Struct.Embed
 
   @impl true
-  def usage, do: ["valid <regex>"]
+  def usage, do: ["valid <regex>", "valid <regex> <flags>"]
 
   @impl true
   def description, do: "Check if this regex is valid."
@@ -14,36 +14,41 @@ defmodule Rewizard.Cogs.Valid do
   @impl true
   def predicates, do: [&Rewizard.Predicates.correct_channel/1, &Rewizard.Predicates.rate_limit/1]
 
-  def failed(regex, message) do
-    %Embed{}
-    |> put_title("Rewizard - Valid")
-    |> put_color(0xFF0000)
-    |> put_field("Regex", "`#{regex}`")
-    |> put_field("Error", message)
-  end
-
   def success(regex) do
     %Embed{}
     |> put_title("Rewizard - Valid")
     |> put_color(0x008000)
-    |> put_field("Regex", "`#{Regex.source(regex)}`")
+    |> put_field("Regex", Rewizard.Regex.source(regex))
     |> put_field("Valid", "Yes.")
+  end
+
+  def failed(strRegex, message) do
+    %Embed{}
+    |> put_title("Rewizard - Valid")
+    |> put_color(0xFF0000)
+    |> put_field("Regex", strRegex)
+    |> put_field("Error", message)
+  end
+
+  def valid(strRegex, flags) do
+    case Rewizard.Regex.compile(strRegex, flags) do
+      {:ok, regex} ->
+        success(regex)
+
+      {:error, strRegex, msg} ->
+        failed(strRegex, msg)
+    end
   end
 
   @impl true
   def command(msg, [regex]) do
-    reply =
-      case Regex.compile(regex) do
-        {:ok, regex} ->
-          success(regex)
+    reply = valid(regex, "")
+    Api.create_message!(msg.channel_id, embed: reply)
+  end
 
-        {:error, {error, location}} ->
-          failed(
-            regex,
-            "Failed to parse regex at location #{location} with error #{inspect(error)}"
-          )
-      end
-
+  @impl true
+  def command(msg, [regex, flags]) do
+    reply = valid(regex, flags)
     Api.create_message!(msg.channel_id, embed: reply)
   end
 end
---
+++ lib/rewizard/regex.ex
@@ -0,0 +1,27 @@
+defmodule Rewizard.Regex do
+  def source(regex) do
+    source(regex.source, Regex.opts(regex))
+  end
+
+  def source(strRegex, flags) do
+    if flags == "" do
+      "`/#{strRegex}/`"
+    else
+      "`/#{strRegex}/` `-#{flags}`"
+    end
+  end
+
+  def compile(regex, flags) do
+    case Regex.compile(regex, flags) do
+      {:ok, regex} ->
+        {:ok, regex}
+
+      {:error, {:invalid_option, flag}} ->
+        {:error, source(regex, flag), "Invalid flag provided: #{flag}"}
+
+      {:error, {error, location}} ->
+        {:error, source(regex, flags),
+         "Failed to parse regex at location #{location} with error #{inspect(error)}"}
+    end
+  end
+end
